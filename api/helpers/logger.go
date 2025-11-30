5ec67334c9b708b8516e8e3f59dc16e493b4d245
Ankit Ydv <ankityadav11241@gmail.com>
2025-11-30 21:19:49 +0530

    logger

---
+++ api/helpers/logger.go
@@ -0,0 +1,33 @@
+package helpers
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+)
+
+func GetCurrentFilePath(file string) string {
+	wd, err := os.Getwd()
+	if err != nil {
+		return file
+	}
+	var currFile string
+	if len(strings.Split(file, wd)) > 1 {
+		currFile = "." + strings.Split(file, wd)[1]
+	} else {
+		currFile = "." + strings.Split(file, wd)[0]
+	}
+	return currFile
+}
+
+func Log(s string) {
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		currentFile := GetCurrentFilePath(file)
+		fmt.Printf("[%s] -> %s:%d: %v\n", time.DateTime, currentFile, line, s)
+	} else {
+		fmt.Printf("[%s] -> %s", time.DateTime, s)
+	}
+}
